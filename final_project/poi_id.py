f55528c5fe20222c31226a68d307adb59b246a42
Akihiro Nakao <genn777f3@gmail.com>
2018-01-18 11:10:09 +0900

    add jupyter notebook

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -12,6 +12,9 @@ from tester import dump_classifier_and_data
 ### The first feature must be "poi".
 features_list = ['poi','salary'] # You will need to use more features
 
+### Task 1.5: Feature Engineering
+
+
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
@@ -35,11 +38,11 @@ labels, features = targetFeatureSplit(data)
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
